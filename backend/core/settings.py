3fb68f1786fb3d71e1adef23cb3fdc70a5b01986
Haris Fayyaz <mhf.haris56@gmail.com>
2026-06-17 02:28:53 +0500

    feat: Configure Django media files for avatar uploads
    - Add MEDIA_URL='media/' and MEDIA_ROOT to settings
    - Enable media serving in development via urls.py
    - Files stored at backend/media/ and served at /media/

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -90,6 +90,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Allow React frontend to talk to Django
